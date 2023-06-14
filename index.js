1a5edc9dcd3378b649d1016f285f5fdc50dda55c
Esther <11yekinie@gmail.com>
2023-06-14 08:05:52 +0100

    player assignation

--- index.js
+++ index.js
@@ -1,69 +1,88 @@
-const gameBoard(board) {
-    const board = [];
-    const createBoard = () => {
-        for (let i = 0; i < 9; i++) {
-            board.push('');
-        }
+const gameBoard = () => {
+  const board = [];
+  const createBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      board.push('');
     }
-    const getBoard = () => board;
-    const updateBoard = (index, player) => {
-        board[index] = player.getSymbol();
+  };
+  const getBoard = () => board;
+  const updateBoard = (index, player) => {
+    board[index] = player.getSymbol();
+  };
+  const resetBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      board[i] = '';
     }
-    const resetBoard = () => {
-        for (let i = 0; i < 9; i++) {
-            board[i] = '';
-        }
-    }
-    return { createBoard, getBoard, updateBoard, resetBoard }
-}
+  };
+  return { createBoard, getBoard, updateBoard, resetBoard };
+};
 
-const playerFactory(name, symbol) {
-    const getName = () => name;
-    const getSymbol = () => symbol;
-    return { getName, getSymbol }
-}
+const playerFactory = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+};
 
-const gameController(player1, player2, board) {
-    player1 = playerFactory('Player 1', 'X');
-    player2 = playerFactory('Player 2', 'O');
-    board = gameBoard();
-    const startGame = () => {
-        board.createBoard();
-    }
-    const getBoard = () => board.getBoard();
-    const updateBoard = (index, player) => {
-        board.updateBoard(index, player);
-    }
-    const resetBoard = () => {
-        board.resetBoard();
-    }
-    return { startGame, getBoard, updateBoard, resetBoard }
-}
+const gameController = (player1, player2, board) => {
+  player1 = playerFactory('Player 1', 'X');
+  player2 = playerFactory('Player 2', 'O');
+  board = gameBoard();
+  const startGame = () => {
+    board.createBoard();
+  };
+  const getBoard = () => board.getBoard();
+  const updateBoard = (index, player) => {
+    board.updateBoard(index, player);
+  };
+  const resetBoard = () => {
+    board.resetBoard();
+  };
+  return {
+    startGame, getBoard, updateBoard, resetBoard, player1, player2,
+  };
+};
 
 const displayController = (() => {
-    const game = gameController();
-    const board = game.getBoard();
-    const boxes = document.querySelectorAll('.box');
-    const startGame = () => {
-        game.startGame();
-        boxes.forEach(box => {
-            box.addEventListener('click', (e) => {
-                const index = e.target.getAttribute('data-index');
-                game.updateBoard(index, player1.getSymbol());
-                displayController.updateBoard();
-            })
-        })
-    }
-    const updateBoard = () => {
-        board = game.getBoard();
-        boxes.forEach((box, index) => {
-            box.textContent = board[index];
-        })
-    }
-    return { startGame, updateBoard }
+  const game = gameController();
+  const boxes = document.querySelectorAll('.box');
+  const startGame = () => {
+    game.startGame();
+    boxes.forEach((box, index) => {
+      box.addEventListener('click', (e) => {
+        index = e.target.getAttribute('data-index');
+        game.updateBoard(index, game.player1);
+        box.textContent = game.getBoard()[index];
+      });
+    });
+  };
+  return { startGame };
 })();
+
 displayController.startGame();
-```
 
-## 3.2.2. Tic Tac Toe - DOM Manipulation
+function computerPlayer() {
+  const boxes = document.querySelectorAll('.box');
+  const game = gameController();
 
+  document.querySelector('#computer').addEventListener('change', function() {
+    if (this.checked) {
+      document.querySelector('#p2').disabled = true;
+      game.player2 = playerFactory('Computer', 'O');
+      computerMove();
+    } else {
+      document.querySelector('#p2').disabled = false;
+      game.player2 = playerFactory('Player 2', 'O');
+    }
+  });
+
+  function computerMove() {
+    let randomIndex = Math.floor(Math.random() * 9);
+    while (game.getBoard()[randomIndex] !== '') {
+      randomIndex = Math.floor(Math.random() * 9);
+    }
+    game.updateBoard(randomIndex, game.player2);
+    boxes[randomIndex].textContent = game.getBoard()[randomIndex];
+  }
+}
+
+computerPlayer();
